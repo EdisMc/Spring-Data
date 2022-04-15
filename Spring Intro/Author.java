f083946ae575719d6387dee08d1469fb28ff5ef9
Edis Reyhan <92871901+EdisMc@users.noreply.github.com>
2022-04-15 21:59:48 +0300

    Add files via upload

---
+++ Spring Intro/Author.java
@@ -0,0 +1,39 @@
+package com.example.springintro.models.entities;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table
+public class Author extends BaseEntity {
+
+    private String firstName;
+    private String lastName;
+
+    public Author() {
+    }
+
+    public Author(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    @Column(name = "first_name")
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    @Column(name = "last_name", nullable = false)
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+}
---
+++ Spring Intro/EditionType.java
@@ -0,0 +1,5 @@
+package com.example.springintro.models.entities;
+
+public enum EditionType {
+    NORMAL, PROMO, GOLD
+}
