05a95a98088426ee2dd4c28ebb09ccd9abbb3e11
Edis Reyhan <92871901+EdisMc@users.noreply.github.com>
2022-05-16 18:20:15 +0300

    Add files via upload

--- UserRepository.java
+++ UserRepository.java
@@ -1,23 +1,9 @@
-package com.example.jsonex.productshop.repositories;
+package com.example.workshop1.repositories;
 
-import com.example.jsonex.productshop.entities.users.User;
-import com.example.jsonex.productshop.entities.users.UserWithSoldProductsDTO;
+import com.example.workshop1.models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
-
 @Repository
-public interface UserRepository extends JpaRepository<User, Integer> {
-    @Query("SELECT u FROM User u" +
-            " JOIN u.sellingItems p" +
-            " WHERE p.buyer IS NOT NULL")
-    List<User> findAllWithSoldProducts();
-
-    @Query("SELECT u FROM User u" +
-            " JOIN u.sellingItems p" +
-            " WHERE p.buyer IS NOT NULL" +
-            " ORDER BY size(u.sellingItems) DESC, u.lastName ASC")
-    List<User> findAllWithSoldProductsOrderByCount();
+public interface UserRepository extends JpaRepository<User, Long> {
 }
