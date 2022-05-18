be3f4f6d3e3ac09e2867ae1765d4296d94c0d448
Edis Reyhan <92871901+EdisMc@users.noreply.github.com>
2022-05-18 22:58:12 +0300

    Add files via upload

--- UserRepository.java
+++ UserRepository.java
@@ -1,9 +1,12 @@
-package com.example.workshop1.repositories;
+package com.example.gamestore.repositories;
 
-import com.example.workshop1.models.User;
+import com.example.gamestore.entities.users.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
-public interface UserRepository extends JpaRepository<User, Long> {
+public interface UserRepository extends JpaRepository<User, Integer> {
+    Optional<User> findByEmailAndPassword(String email, String password);
 }
