213023cca704d92a2ad3ca9fa015fcbb2df444c7
Edis Reyhan <92871901+EdisMc@users.noreply.github.com>
2022-05-20 21:33:22 +0300

    Add files via upload

--- CategoryRepository.java
+++ CategoryRepository.java
@@ -1,9 +1,12 @@
-package com.example.jsonex.productshop.repositories;
+package com.example.demo.repositories;
 
-import com.example.jsonex.productshop.entities.categories.Category;
+import com.example.demo.entities.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
-public interface CategoryRepository extends JpaRepository<Category, Integer> {
+public interface CategoryRepository
+        extends JpaRepository<Category, Integer> {
 }
