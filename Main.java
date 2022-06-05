4c9b843e788c5d912cabfef76dd27a00f70b035f
Edis Reyhan <92871901+EdisMc@users.noreply.github.com>
2022-06-05 21:23:30 +0300

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,56 +1,24 @@
-import entities.shampoo.BasicIngredient;
-import entities.shampoo.BasicLabel;
-import entities.shampoo.BasicShampoo;
-import entities.shampoo.ProductionBatch;
-import entities.vehicle.Bike;
-import entities.vehicle.Car;
-import entities.vehicle.Truck;
-
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
-import java.time.LocalDate;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Set;
 
 public class Main {
 
     public static void main(String[] args) {
         EntityManagerFactory factory =
-                Persistence.createEntityManagerFactory("PU_Name");
+                Persistence.createEntityManagerFactory("CodeFirstEx");
 
         EntityManager entityManager = factory.createEntityManager();
         entityManager.getTransaction().begin();
 
-        ProductionBatch batch = new ProductionBatch(LocalDate.now());
-        BasicLabel label = new BasicLabel("blue");
-        BasicShampoo shampoo = new BasicShampoo("shower", label, batch);
+        User user = new User("marina", AccountType.PLATINUM);
+        entityManager.persist(user);
 
-        BasicIngredient ingredient = new BasicIngredient(100, "B12");
-        BasicIngredient ingredient2 = new BasicIngredient(2, "Violet");
+        User found = entityManager.find(User.class, 1);
 
-        shampoo.addIngredient(ingredient);
-        shampoo.addIngredient(ingredient2);
-
-        List<String> names = Arrays.asList("pesho", "gosho", "alex");
-        shampoo.setNames(names);
-
-        entityManager.persist(ingredient);
-        entityManager.persist(ingredient2);
-
-        entityManager.persist(batch);
-        entityManager.persist(label);
-        entityManager.persist(shampoo);
-
-        ProductionBatch productionBatch = entityManager.find(ProductionBatch.class, 1);
-
-//        Set<BasicShampoo> shampoos = productionBatch.getShampoos();
-//
-//        shampoos.forEach(System.out::println);
+        System.out.println(found.getType().toString());
 
         entityManager.getTransaction().commit();
-
-
+        entityManager.close();
     }
 }
