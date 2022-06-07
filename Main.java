deb035232ce8e4a1e68ca7833596f7f56d535c1c
Edis Reyhan <92871901+EdisMc@users.noreply.github.com>
2022-06-07 22:17:16 +0300

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,24 +1,45 @@
-import javax.persistence.EntityManager;
-import javax.persistence.EntityManagerFactory;
-import javax.persistence.Persistence;
+import entities.Address;
+import entities.User;
+import orm.EntityManager;
+
+import java.lang.reflect.InvocationTargetException;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.time.LocalDate;
+import java.util.List;
+
+import static orm.MyConnector.createConnection;
+import static orm.MyConnector.getConnection;
 
 public class Main {
 
-    public static void main(String[] args) {
-        EntityManagerFactory factory =
-                Persistence.createEntityManagerFactory("CodeFirstEx");
+    public static void main(String[] args) throws SQLException, IllegalAccessException, InvocationTargetException, NoSuchMethodException, InstantiationException {
+        createConnection("root", "", "custom-orm");
+        Connection connection = getConnection();
 
-        EntityManager entityManager = factory.createEntityManager();
-        entityManager.getTransaction().begin();
+        EntityManager<User> userEntityManager = new EntityManager<>(connection);
+        EntityManager<Address> addressEntityManager = new EntityManager<>(connection);
 
-        User user = new User("marina", AccountType.PLATINUM);
-        entityManager.persist(user);
+        addressEntityManager.doCreate(Address.class);
 
-        User found = entityManager.find(User.class, 1);
+        User user = new User("pesho", 25, LocalDate.now());
+        user.setUsername("pesho_new_finalv2");
 
-        System.out.println(found.getType().toString());
+//        userEntityManager.doCreate(User.class);
+//        userEntityManager.doAlter(User.class);
+        userEntityManager.persist(user);
 
-        entityManager.getTransaction().commit();
-        entityManager.close();
+        Iterable<User> first = userEntityManager.find(User.class);
+        System.out.println(first.toString());
+
+        User toDelete = userEntityManager.findFirst(User.class, "id > 3");
+        System.out.println(toDelete);
+
+        userEntityManager.delete(toDelete);
+
+        Iterable<User> second = userEntityManager.find(User.class);
+        System.out.println(second.toString());
+
+        connection.close();
     }
 }
