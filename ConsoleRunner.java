684a5b2ddd41fe4762a4aa10b73149d35373364b
Edis Reyhan <92871901+EdisMc@users.noreply.github.com>
2022-06-09 21:38:00 +0300

    Add files via upload

--- ConsoleRunner.java
+++ ConsoleRunner.java
@@ -1,38 +1,22 @@
-package com.example.gamestore;
+package com.example._11_spring_data_lab;
 
-import com.example.gamestore.exceptions.UserNotLoggedInException;
-import com.example.gamestore.exceptions.ValidationException;
-import com.example.gamestore.services.ExecutorService;
+import com.example._11_spring_data_lab.models.User;
+import com.example._11_spring_data_lab.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
-import java.util.Scanner;
-
 @Component
 public class ConsoleRunner implements CommandLineRunner {
-
-    private final ExecutorService executorService;
-
     @Autowired
-    public ConsoleRunner(ExecutorService executorService) {
-        this.executorService = executorService;
-    }
+    private UserService userService;
 
     @Override
-    public void run(String... args) {
-        Scanner scanner = new Scanner(System.in);
+    public void run(String... args) throws Exception {
+        User first = new User("qna", 22);
+        userService.registerUser(first);
 
-        String command = scanner.nextLine();
-        String commandData = scanner.nextLine();
-
-        String result;
-        try {
-            result = executorService.execute(command, commandData);
-        } catch (ValidationException | UserNotLoggedInException ex) {
-            result = ex.getMessage();
-        }
-
-        System.out.println(result);
+        User second = new User("qna", 22);
+        userService.registerUser(second);
     }
 }
